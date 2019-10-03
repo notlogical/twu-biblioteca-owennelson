83ce00a6ca7c60f4ff50bc617fbefe3b6c7beefc
Owen Nelson <onelson@berkeley.edu>
2019-10-03 11:46:25 -0500

    Added class to test outputs and class to store utility text.

---
+++ test/com/twu/biblioteca/OutputTests.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/** Test suite for outputs to console from BibliotecaApp. */
+
+public class OutputTests {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originalOutput = System.out;
+    BibliotecaApp biblioteca;
+
+
+    @Before
+    public void init() {
+        System.setOut(new PrintStream(output));
+        biblioteca = new BibliotecaApp();
+    }
+
+    @After
+    public void shutdown() {
+        System.setOut(originalOutput);
+    }
+
+    @Test
+    public void testWelcomeMessage() {
+        assertThat(output.toString(),is("Welcome to "));
+    }
+}
