6dab778e67470c4a3043c2044aade77a65541a16
Owen Nelson <onelson@berkeley.edu>
2019-10-04 10:48:01 -0500

    Implemented story 1.3

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class BibliotecaApp {
 
     private static List<Book> _books;
+    private static int fieldWidth = 30;
 
     public static void main(String[] args) {
         loadBooks();
@@ -15,11 +16,14 @@ public class BibliotecaApp {
 
     private static void loadBooks() {
         _books = new ArrayList<>();
-        _books.add(new Book("Bosch Automotive Handbook"));
-        _books.add(new Book("Clean Code"));
-        _books.add(new Book("Modern Global Seismology"));
-        _books.add(new Book("One Hundred Years of Solitude"));
-        _books.add(new Book("Sled Driver"));
+        _books.add(new Book("Bosch Automotive Handbook", "Robert Bosch GmbH",
+                1932));
+        _books.add(new Book("Clean Code", "Robert C. Martin", 2008));
+        _books.add(new Book("Modern Global Seismology", "Thorne Lay & Terry " +
+                "C. Wallace", 1995));
+        _books.add(new Book("One Hundred Years of Solitude", "Gabriel Garcia " +
+                "Marquez", 1967));
+        _books.add(new Book("Sled Driver", "Brian Shul", 1991));
     }
 
     private static void printBookList(List<Book> books) {
@@ -29,6 +33,23 @@ public class BibliotecaApp {
     }
 
     private static void printBook(Book book) {
-        System.out.println(book.getTitle());
+        System.out.println(fitText(book.getTitle()) + "|" + fitText(book
+                .getAuthor()) + "|" + book.getYear());
+    }
+
+    private static String fitText(String text) {
+        if (text.length() > fieldWidth) {
+            return trimText(text);
+        } else {
+            return padText(text);
+        }
+    }
+
+    private static String trimText(String text) {
+        return text.substring(0, 27) + "...";
+    }
+
+    private static String padText(String text) {
+        return text.format("%-" + fieldWidth + "s", text);
     }
 }
--- src/com/twu/biblioteca/Utils.java
+++ src/com/twu/biblioteca/Utils.java
@@ -6,14 +6,14 @@ public class Utils {
             "one-stop-shop for great book titles in Bangalore!";
 
     public static final String BOOK_LIST =
-            "Bosch Automotive Handbook      | Robert Bosch GmbH             " +
-                    "| 1932\n" +
-            "Clean Code                     | Robert C. Martin              " +
-                    "| 2008\n" +
-            "Modern Global Seismology       | Thorne Lay & Terry C. Wallace " +
-                    "| 1995\n" +
-            "One Hundred Years of Solitude  | Gabriel Garcia Marquez        " +
-                    "| 1967\n" +
-            "Sled Driver                    | Brian Shul                    " +
-                    "| 1991\n";
+            "Bosch Automotive Handbook     |Robert Bosch GmbH             " +
+                    "|1932\n" +
+            "Clean Code                    |Robert C. Martin              " +
+                    "|2008\n" +
+            "Modern Global Seismology      |Thorne Lay & Terry C. Wallace " +
+                    "|1995\n" +
+            "One Hundred Years of Solitude |Gabriel Garcia Marquez        " +
+                    "|1967\n" +
+            "Sled Driver                   |Brian Shul                    " +
+                    "|1991\n";
 }
