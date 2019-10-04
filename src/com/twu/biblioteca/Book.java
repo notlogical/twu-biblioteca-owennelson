6f54fd36be221f19110328c4eed4c0c01cc28614
Owen Nelson <onelson@berkeley.edu>
2019-10-04 10:20:07 -0500

    Added fields in Book class for author and year

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -2,12 +2,24 @@ package com.twu.biblioteca;
 
 public class Book {
     private String _title;
+    private String _author;
+    private int _year;
 
-    Book(String title) {
+    Book(String title, String author, int year) {
         _title = title;
+        _author = author;
+        _year = year;
     }
 
     public String getTitle() {
         return _title;
+    }
+
+    public String getAuthor() {
+        return _author;
+    }
+
+    public int getYear() {
+        return _year;
     }
 }
