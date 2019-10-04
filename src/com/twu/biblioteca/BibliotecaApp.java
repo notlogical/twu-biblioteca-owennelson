8583da97ebecf9395898ea7e7aec853fb84e4747
Owen Nelson <onelson@berkeley.edu>
2019-10-04 10:00:25 -0500

    Added book class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,28 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+
 public class BibliotecaApp {
 
+    private static List<Book> _books;
+
     public static void main(String[] args) {
+        loadBooks();
         System.out.println(Utils.WELCOME_TEXT);
+        printBookList(_books);
+    }
+
+    private static void loadBooks() {
+
+    }
+
+    private static void printBookList(List<Book> books) {
+        for (Book book : books) {
+            printBook(book);
+        }
+    }
+
+    private static void printBook(Book book) {
+        System.out.println(book.title);
     }
 }
---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class Book {
+    private String _title;
+
+    Book(String title) {
+        _title = title;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+}
