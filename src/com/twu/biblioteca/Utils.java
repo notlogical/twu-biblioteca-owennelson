98d42316868681dd2d15a27f7eb82843de09d9d3
Owen Nelson <onelson@berkeley.edu>
2019-10-04 10:17:29 -0500

    Modified test for list of books to look for Author and Publication Year

--- src/com/twu/biblioteca/Utils.java
+++ src/com/twu/biblioteca/Utils.java
@@ -6,9 +6,14 @@ public class Utils {
             "one-stop-shop for great book titles in Bangalore!";
 
     public static final String BOOK_LIST =
-            "Bosch Automotive Handbook\n" +
-            "Clean Code\n" +
-            "Modern Global Seismology\n" +
-            "One Hundred Years of Solitude\n" +
-            "Sled Driver\n";
+            "Bosch Automotive Handbook      | Robert Bosch GmbH             " +
+                    "| 1932\n" +
+            "Clean Code                     | Robert C. Martin              " +
+                    "| 2008\n" +
+            "Modern Global Seismology       | Thorne Lay & Terry C. Wallace " +
+                    "| 1995\n" +
+            "One Hundred Years of Solitude  | Gabriel Garcia Marquez        " +
+                    "| 1967\n" +
+            "Sled Driver                    | Brian Shul                    " +
+                    "| 1991\n";
 }
