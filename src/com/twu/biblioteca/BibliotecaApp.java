fb13220ca73b7477e41c3e204e2f8c0896fb6f79
Owen Nelson <onelson@berkeley.edu>
2019-10-04 10:58:20 -0500

    All printing methods extracted to new ConsolePrinter class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,12 +6,11 @@ import java.util.List;
 public class BibliotecaApp {
 
     private static List<Book> _books;
-    private static int fieldWidth = 30;
 
     public static void main(String[] args) {
         loadBooks();
         ConsolePrinter.printWelcome();
-        printBookList(_books);
+        ConsolePrinter.printBookList(_books);
     }
 
     private static void loadBooks() {
@@ -26,30 +25,4 @@ public class BibliotecaApp {
         _books.add(new Book("Sled Driver", "Brian Shul", 1991));
     }
 
-    private static void printBookList(List<Book> books) {
-        for (Book book : books) {
-            printBook(book);
-        }
-    }
-
-    private static void printBook(Book book) {
-        System.out.println(fitText(book.getTitle()) + "|" + fitText(book
-                .getAuthor()) + "|" + book.getYear());
-    }
-
-    private static String fitText(String text) {
-        if (text.length() > fieldWidth) {
-            return trimText(text);
-        } else {
-            return padText(text);
-        }
-    }
-
-    private static String trimText(String text) {
-        return text.substring(0, 27) + "...";
-    }
-
-    private static String padText(String text) {
-        return text.format("%-" + fieldWidth + "s", text);
-    }
 }
--- src/com/twu/biblioteca/ConsolePrinter.java
+++ src/com/twu/biblioteca/ConsolePrinter.java
@@ -1,4 +1,5 @@
 package com.twu.biblioteca;
+import java.util.List;
 
 public class ConsolePrinter {
     private static int fieldWidth = 30;
@@ -7,4 +8,30 @@ public class ConsolePrinter {
         System.out.println(Utils.WELCOME_TEXT);
     }
 
+    public static void printBookList(List<Book> books) {
+        for (Book book : books) {
+            printBook(book);
+        }
+    }
+
+    private static void printBook(Book book) {
+        System.out.println(fitText(book.getTitle()) + "|" + fitText(book
+                .getAuthor()) + "|" + book.getYear());
+    }
+
+    private static String fitText(String text) {
+        if (text.length() > fieldWidth) {
+            return trimText(text);
+        } else {
+            return padText(text);
+        }
+    }
+
+    private static String trimText(String text) {
+        return text.substring(0, 27) + "...";
+    }
+
+    private static String padText(String text) {
+        return text.format("%-" + fieldWidth + "s", text);
+    }
 }
