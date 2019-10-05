a421537b9cf3429dd345d28e610f95f8127da060
Owen Nelson <onelson@berkeley.edu>
2019-10-05 11:36:39 -0500

    Made new abstract class Media for Books and Movies to extend

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,45 +1,14 @@
 package com.twu.biblioteca;
 
-public class Book {
-    private String _title;
+class Book extends Media {
     private String _author;
-    private int _year;
-    private int _quantityAvailable;
-    private int _maxQuantity;
 
     Book(String title, String author, int year) {
-        _title = title;
+        super(title, year);
         _author = author;
-        _year = year;
-        _quantityAvailable = 1;
-        _maxQuantity = 1;
     }
 
-    public String getTitle() {
-        return _title;
-    }
-
-    public String getAuthor() {
+    String getAuthor() {
         return _author;
-    }
-
-    public int getYear() {
-        return _year;
-    }
-
-    public boolean isAvailable() {
-        return _quantityAvailable > 0;
-    }
-
-    public boolean isCheckedOut() {
-        return _quantityAvailable < _maxQuantity;
-    }
-
-    void checkout() {
-        _quantityAvailable--;
-    }
-
-    void returnBook() {
-        _quantityAvailable++;
     }
 }
---
+++ src/com/twu/biblioteca/Media.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+abstract class Media {
+    private String _title;
+    private int _year;
+    private int _quantityAvailable;
+    private int _maxQuantity;
+
+    Media(String title, int year) {
+        _title = title;
+        _year = year;
+        _quantityAvailable = 1;
+        _maxQuantity = 1;
+    }
+
+    String getTitle() {
+        return _title;
+    }
+
+    int getYear() {
+        return _year;
+    }
+
+    boolean isAvailable() {
+        return _quantityAvailable > 0;
+    }
+
+    boolean isCheckedOut() {
+        return _quantityAvailable < _maxQuantity;
+    }
+
+    void checkout() {
+        _quantityAvailable--;
+    }
+
+    void returnBook() {
+        _quantityAvailable++;
+    }
+}
