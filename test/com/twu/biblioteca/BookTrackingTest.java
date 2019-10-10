02d6943206d3c051667fca818cff40869cb8a155
Owen Nelson <owen.nelson@thoughtworks.com>
2019-10-10 11:19:45 -0500

    Removed unnecessary input/output from BookTrackingTests

--- test/com/twu/biblioteca/BookTrackingTest.java
+++ test/com/twu/biblioteca/BookTrackingTest.java
@@ -1,12 +1,7 @@
 package com.twu.biblioteca;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 
 import static com.twu.biblioteca.TestingUtils.*;
 import static org.junit.Assert.assertThat;
@@ -14,24 +9,14 @@ import static org.junit.Assert.assertNull;
 import static org.hamcrest.core.Is.is;
 
 public class BookTrackingTest {
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    private final PrintStream originalOutput = System.out;
-    private final InputStream originalInput = System.in;
     private Library theLibrary;
 
     @Before
     public void init() {
-        //System.setOut(new PrintStream(output));
         theLibrary = new Library();
         loadBooks(theLibrary);
         loadMovies(theLibrary);
     }
-
-    /*@After
-    public void shutdown() {
-        System.setOut(originalOutput);
-        System.setIn(originalInput);
-    }*/
 
     @Test
     public void testIfCheckoutLogged() {
